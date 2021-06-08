35f9cd25e40674da5f72e2659653852d25a809e0
Sayed M. Hussein <sayed.hussein@gmx.com>
2021-06-08 21:55:11 +0300

    Adjusted Project src Structure

---
+++ src/Server/WebApi/Options/ApiVersionOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+
+namespace MySystem.Web.Api.Options
+{
+    public class ApiVersionOptions
+    {
+        public static void ConfigureApiVersioning(ApiVersioningOptions options)
+        {
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.ApiVersionReader = new MediaTypeApiVersionReader();
+            options.ReportApiVersions = true;
+        }
+    }
+}
---
+++ src/Server/WebApi/Startup.cs
@@ -0,0 +1,117 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Application;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MySystem.Api.Services;
+using MySystem.Application.Common.Interfaces;
+using MySystem.Infrastructure;
+using MySystem.MySystem.Api.Middleware;
+using MySystem.MySystem.Api.Service;
+using MySystem.Persistence;
+using MySystem.SharedKernel.Interfaces;
+using MySystem.SharedKernel.Services;
+using MySystem.Web.Api.Options;
+using MySystem.Web.Api.Security.TokenRefreshment;
+using MySystem.Web.Api.Services;
+
+namespace MySystem.Web.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            services.AddHttpContextAccessor();
+
+            services.AddApplication();
+            services.AddInfrastructure(Configuration);
+            services.AddPersistence(Configuration);
+
+            services.AddApiVersioning(ApiVersionOptions.ConfigureApiVersioning);
+            services.AddSwaggerGen(SwaggerOptions.ConfigureSwaggerGen);
+
+            services.AddSingleton<ISharedValues, SharedValues>();
+
+            services.AddScoped<ICurrentUserInfo, UserInfoService>();
+            services.AddSingleton<IJwtService>(provider => new JwtService(Configuration["Jwt:Key"]));
+
+            AuthenticationConfig(services);
+
+
+            //AuthorizationConfig(services);
+            services.AddSingleton<IAuthorizationHandler, TokenRefreshmentHandler>();
+            services.AddAuthorization(AuthorOptions.ConfigureAuthOptions);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(SwaggerOptions.ConfigureSwaggerUI);
+            }
+            else
+            {
+                app.UseHttpsRedirection();
+            }
+
+            app.UseCustomExceptionHandler();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private void AuthenticationConfig(IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new JwtService(Configuration["Jwt:Key"]).ValidationParameters;
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        try
+                        {
+                            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+                            context.Token = token;
+                        }
+                        catch { }
+
+                        return Task.CompletedTask;
+                    },
+                };
+            });
+        }
+    }
+}
