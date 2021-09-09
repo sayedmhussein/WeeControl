f7729cfb799a963e374131809a6702945f3fd537
Sayed M. Hussein <sayed.hussein@gmx.com>
2021-09-10 01:25:15 +0300

    Added Mudblazor

--- src/Common/SharedKernel/Aggregates/Employee/DtosV1/CreateLoginDto.cs
+++ src/Common/SharedKernel/Aggregates/Employee/DtosV1/CreateLoginDto.cs
@@ -6,6 +6,7 @@ namespace WeeControl.SharedKernel.Aggregates.Employee.DtosV1
     public class CreateLoginDto : IEntityDto
     {
         [Required]
+        [MinLength(3, ErrorMessage = "Minimum username is 3 letters!")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "Minimum Username is 3 letters")]
         public string Username { get; set; }
 
--- src/Frontend/Wasm/Program.cs
+++ src/Frontend/Wasm/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 
 namespace Wasm
 {
@@ -16,7 +17,7 @@ namespace Wasm
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
+            builder.Services.AddMudServices(); // https://mudblazor.com/
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
