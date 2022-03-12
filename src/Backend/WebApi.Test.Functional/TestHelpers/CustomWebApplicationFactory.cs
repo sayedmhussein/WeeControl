f4913cc31b32fb066198797d5a0dfd0f79449e15
Sayed M. Hussein <sayed.hussein@gmx.com>
2022-03-12 21:25:25 +0300

    Removed Unnecessary using

--- src/Backend/WebApi.Test.Functional/TestHelpers/CustomWebApplicationFactory.cs
+++ src/Backend/WebApi.Test.Functional/TestHelpers/CustomWebApplicationFactory.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
-using Microsoft.Extensions.DependencyInjection;
-using WeeControl.Backend.Domain.Credentials;
 using WeeControl.Backend.Persistence;
-using WeeControl.Backend.Persistence.Credentials;
 using Xunit;
 
 [assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true, MaxParallelThreads = 1)]
