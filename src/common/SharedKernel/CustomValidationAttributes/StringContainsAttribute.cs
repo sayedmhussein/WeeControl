9495cea266609442225f569ad4dd701a0d12f106
Sayed M. Hussein <sayed.hussein@gmx.com>
2023-02-28 07:24:08 +0300

    Adding Validation Attributes.

---
+++ src/common/SharedKernel/CustomValidationAttributes/StringContainsAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeeControl.Core.SharedKernel.CustomValidationAttributes;
+
+public class StringContainsAttribute : ValidationAttribute
+{
+    private const string allowedCharacters = "abcdefghijklmnopqrstuvwxyz1234567890_";
+    
+    public string? Accept { get; set; }
+    public string? Reject { get; set; }
+
+    public override bool IsValid(object? value)
+    {
+        var str = (string)value;
+        
+        if (string.IsNullOrEmpty(str))
+        {
+            return true;
+        }
+
+        foreach (var c in str.ToLower())
+        {
+            if (allowedCharacters.Contains(c) == false)
+            {
+                return false;
+            }
+        }
+        
+        return base.IsValid(value);
+    }
+
+    public override string FormatErrorMessage(string name)
+    {
+        return "Must Contains.";
+    }
+}
---
+++ tests/Core.Test/SharedKernel/CustomValidationAttributes/StringContainsAttributeTests.cs
@@ -0,0 +1,34 @@
+﻿using WeeControl.Core.SharedKernel;
+using WeeControl.Core.SharedKernel.CustomValidationAttributes;
+using WeeControl.Core.SharedKernel.Interfaces;
+
+namespace WeeControl.Core.Test.SharedKernel.CustomValidationAttributes;
+
+public class StringContainsAttributeTests
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData("NoSpace")]
+    [InlineData("AllowedOK")]
+    [InlineData("Allowed1234")]
+    public void WhenContainsValidValue_NoError(string str)
+    {
+        var test = new TestObject() {TestString = str};
+
+        Assert.True(test.IsValidEntityModel());
+    }
+    
+    [Fact]
+    public void WhenContainsInvalidValidValue_ThrowError()
+    {
+        var test = new TestObject() { TestString = "Allowed ! OK"};
+
+        Assert.Throws<EntityModelValidationException>(() => test.ThrowExceptionIfEntityModelNotValid());
+    }
+}
+
+internal class TestObject : IEntityModel
+{
+    [StringContains]
+    public string TestString { get; set; } = string.Empty;
+}
