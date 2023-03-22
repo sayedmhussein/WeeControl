6213b40c64469f73300878a305d14a3623fe6b65
Sayed M. Hussein <sayed.hussein@gmx.com>
2023-03-22 11:29:42 +0300

    Rafactors.

--- src/common/SharedKernel/Models/CityModel.cs
+++ src/common/SharedKernel/Models/CityModel.cs
@@ -4,10 +4,10 @@ namespace WeeControl.Core.SharedKernel.Models;
 
 public class CityModel
 {
-    public string Name { get; init; }
+    public string Name { get; init; }  = string.Empty;
     
     [StringLength(3, MinimumLength = 3)]
-    public string Code3 { get; init; }
+    public string Code3 { get; init; }  = string.Empty;
     
-    public string Local { get; init; }
+    public string Local { get; init; }  = string.Empty;
 }
--- src/common/SharedKernel/Models/CountryModel.cs
+++ src/common/SharedKernel/Models/CountryModel.cs
@@ -4,18 +4,18 @@ namespace WeeControl.Core.SharedKernel.Models;
 
 public class CountryModel
 {
-    public string Name { get; init; }
+    public string Name { get; init; } = string.Empty;
     
     [StringLength(2, MinimumLength = 2)]
-    public string Code2 { get; init; }
+    public string Code2 { get; init; } = string.Empty;
     
     [StringLength(3, MinimumLength = 3)]
-    public string Code3 { get; init; }
+    public string Code3 { get; init; } = string.Empty;
     
     [Range(1, 999)]
     public int Numeric { get; init; } 
     
-    public string Local { get; }
+    public string Local { get; } = string.Empty;
 
     public IEnumerable<CityModel> Cities { get; init; }
 }
